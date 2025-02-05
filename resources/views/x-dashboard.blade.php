8c9629872181388f18ffe3a1821aef8a508c4305
Vivid Darshan <vivid.darshandudheliya@gmail.com>
2025-02-05 17:38:26 +0530

    home page static upload

---
+++ resources/views/x-dashboard.blade.php
@@ -0,0 +1,25 @@
+<x-header :title="'Dashboard'" />
+
+<div class="container mt-5">
+    <h2>Dashboard</h2>
+
+    @if (session()->has('message'))
+        <div class="alert {{ session('success') ? 'alert-success' : 'alert-danger' }}" role="alert">
+            {{ session('message') }}
+        </div>
+    @endif
+
+    <pre>
+        {{ $user }}
+    </pre>
+
+
+    <!--Buttons: Login | Signup | Logout -->
+    <div class="mt-4">
+        <a href="{{ route('view.login') }}" class="btn btn-primary">Login</a>
+        <a href="{{ route('view.signup') }}" class="ml-2 btn btn-success">Signup</a>
+        <a href="{{ route('auth.logout') }}" class="ml-2 btn btn-danger">Logout</a>
+    </div>
+</div>
+
+<x-footer />
