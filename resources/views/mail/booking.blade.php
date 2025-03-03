3c5291d26c37094267b07dd7455882e02a286e6f
Darshan-Vivid <vivid.darshandudheliya@gmail.com>
2025-03-03 15:51:39 +0530

    mail update

--- resources/views/mail/booking.blade.php
+++ resources/views/mail/booking.blade.php
@@ -1,1 +1,9 @@
 {{ $mailData }}
+@php
+    $booking = App\Models\Booking::find($mailData); 
+    $transaction =  App\Models\Transaction::where('transaction_id', $booking->transaction_id)->first();
+@endphp
+<pre>
+    <?php var_dump($booking)?>
+    <?php var_dump($transaction)?>
+</pre>
